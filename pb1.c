b99de016a155fea23b4c171319bb14b73455eaa1
ilinca andrei <andrei@gmail.com>
2014-03-09 18:11:23 +0200

    pb1 cu liste SDA

--- pb1.c
+++ pb1.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	printf("hello, world!\n");
+struct lista
+{
+	char key;
+	struct lista *next;
+};
+
+typedef struct lista node;
+
+node *init()
+{
+	node *p;
+	p=(node*)malloc(sizeof(node));
+	printf("zi litera: ");
+	scanf("%c",&p->key);
+	p->next=NULL;
+	return p;
+}
+
+void *add_elem(node* cap_lista)
+{
+	node *p, *q;
+	p=cap_lista;
+	while(p->next!=NULL)
+		p=p->next;
+	q=init();
+	p->next=q;
+}
+
+int main() 
+{
+	node* cap_lista;
+	int n, i;
+	cap_lista=init();
+	printf("Zi cate elemente are lista: ");
+	scanf("%d", &n);
+	for(i=1;i<n;i++)
+		add_elem(cap_lista);
+
+
 	return 0;
 }
